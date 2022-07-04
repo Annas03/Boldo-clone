e29950f8a99b70c2362e6c3ccf6e86e935687af4
Annas03 <baigannas9@gmail.com>
2022-07-04 18:06:07 +0500

    eleventh-commit

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -1,3 +1,4 @@
+let mobileNavBarIsClicked = false
 let testimonials = [document.getElementById("review-one"), document.getElementById("review-two"), document.getElementById("review-three")]
 if(screen.width > 1349){
     testimonials[1].style.height = "350px"
@@ -9,12 +10,24 @@ else{
     testimonials[1].style.height = "300px"
 }
 
+
 document.getElementById("move-left").addEventListener("click", leftClicked)
 
 document.getElementById("move-right").addEventListener("click", rightClicked)
 
+document.getElementById("mobile-navbar").addEventListener("click", mobileNavbarClicked)
+
 function leftClicked(){
     testimonials = [testimonials[1], testimonials[2], testimonials[0]]
+    TestimonialsStyles()
+}
+
+function rightClicked(){
+    testimonials = [testimonials[2], testimonials[0], testimonials[1]]
+    TestimonialsStyles()
+}
+
+function TestimonialsStyles(){
     testimonials[0].style.order = "0"
     testimonials[1].style.order = "1"
     testimonials[2].style.order = "2"
@@ -29,18 +42,12 @@ function leftClicked(){
     testimonials[2].style.height = "auto"
 }
 
-function rightClicked(){
-    testimonials = [testimonials[2], testimonials[0], testimonials[1]]
-    testimonials[0].style.order = "0"
-    testimonials[1].style.order = "1"
-    testimonials[2].style.order = "2"
-
-    testimonials[0].style.height = "auto"
-    if(screen.width > 1349){
-        testimonials[1].style.height = "350px"
+function mobileNavbarClicked(){
+    mobileNavBarIsClicked = !mobileNavBarIsClicked
+    if(mobileNavBarIsClicked){
+        document.getElementById("links").style.display = "flex"
     }
     else{
-        testimonials[1].style.height = "300px"
+        document.getElementById("links").style.display = "none"
     }
-    testimonials[2].style.height = "auto"
 }
